e7f3847aa8445782b4e39d9840b37953847ac5ad
Qian Wang <dionwang88@users.noreply.github.com>
2015-09-23 13:56:44 -0400

    Pair

--- database/src/project/Pair.java
+++ database/src/project/Pair.java
@@ -4,7 +4,7 @@ package project;
  * Pair class is to be used to save the index and the data length in the data file
  * @author wangqian
  *
- * @param <L>: Left is the index in data file
+ * @param <L>: Left is the index in meta data file
  * @param <R>: right is the length of the consecutive data
  */
 public class Pair<L,R> {
@@ -21,33 +21,8 @@ public class Pair<L,R> {
 	  public R getRight() { return right; }
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((left == null) ? 0 : left.hashCode());
-		result = prime * result + ((right == null) ? 0 : right.hashCode());
-		return result;
+	public String toString() {
+		return "Pair [left=" + left + ", right=" + right + "]";
 	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Pair other = (Pair) obj;
-		if (left == null) {
-			if (other.left != null)
-				return false;
-		} else if (!left.equals(other.left))
-			return false;
-		if (right == null) {
-			if (other.right != null)
-				return false;
-		} else if (!right.equals(other.right))
-			return false;
-		return true;
-	}
 }
