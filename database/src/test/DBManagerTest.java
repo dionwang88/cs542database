51a55981cd94a843feb8999e0f2a44756534d736
Qian Wang <dionwang88@users.noreply.github.com>
2015-10-01 17:31:20 -0400

    aa

--- database/src/test/DBManagerTest.java
+++ database/src/test/DBManagerTest.java
@@ -1,26 +1,22 @@
 package test;
 
-import java.util.Arrays;
-
 import project.DBManager;
-import project.IndexHelperImpl;
-
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
 
 public class DBManagerTest {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		DBManager dbmanager = DBManager.getInstance();
 		byte[] data = new byte[256 * 1000];
-		for(int i=0;i<data.length;i++){
-			data[i] = (byte) ((i)%127);
+		for (int i = 0; i < data.length; i++) {
+			data[i] = (byte) ((i) % 127);
 		}
-		for (int key = 0; key < 16; key ++){
+		for (int key = 0; key < 16; key++) {
 			dbmanager.Put(key, data);
-			byte[] mydata = dbmanager.Get(0);
+			System.out.println("Put data key: " + key + ", size:" + data.length);
 		}
-			System.out.println("Now loading.");
+		for (int key = 0; key < 1; key++) {
+			byte[] mydata = dbmanager.Get(0);
+			System.out.println("Get data key:" + key + ",size:" + mydata.length);
+		}
 		dbmanager.readDatabase();
-
-}
+	}
 }
