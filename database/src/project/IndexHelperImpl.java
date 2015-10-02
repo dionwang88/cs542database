39454aeabc8b362c31569bc5019fc6c3f341a204
Qian Wang <dionwang88@users.noreply.github.com>
2015-10-01 21:46:00 -0400

    aaa

--- database/src/project/IndexHelperImpl.java
+++ database/src/project/IndexHelperImpl.java
@@ -11,8 +11,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Logger;
-import sun.jvm.hotspot.memory.Space;
-
 
 /**
  * This class provides the methods of how to manipulate the index
