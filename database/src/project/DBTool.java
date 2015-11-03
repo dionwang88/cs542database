c76b890144ef150ac32dde76094abd1fd44d8bfe
Vincent Zhao <zhao.shile@foxmail.com>
2015-11-02 23:17:54 -0500

    clear the class

--- database/src/project/DBTool.java
+++ database/src/project/DBTool.java
@@ -5,7 +5,6 @@ import test.TestConcurrency;
 import test.TestFragmentation;
 import test.TestReadCSV;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
