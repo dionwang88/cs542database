3b11668eb87e0c53b5fbbfd5495c5176d8d1de1b
Vincent Zhao <zhao.shile@foxmail.com>
2015-11-22 14:38:15 -0500

    recovery & updataoperator

---
+++ database/src/project/DBRecovery.java
@@ -0,0 +1,22 @@
+package project;
+
+/**
+ * Created by vincent on 11/22/15.
+ */
+public class DBRecovery {
+    public static void logUpdate(int tid, String attrName, int type, Object oldVal,Object newVal){
+        writeIntoLog();
+    }
+
+    public static Object logLoad(String path){
+        return null;
+    }
+
+    private static void writeIntoLog(String path,String row){
+
+    }
+
+    public static void Recover(){
+
+    }
+}
---
+++ database/src/project/relations/UpdataOperator.java
@@ -0,0 +1,7 @@
+package project.relations;
+
+/**
+ * Created by vincent on 11/22/15.
+ */
+public class UpdataOperator  {
+}
